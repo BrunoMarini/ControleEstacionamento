9e5480a844908ab1f2cac437b7ef6f33e46eff7a
Bruno Guilherme Spirlandeli Marini <bgmarini@hotmail.com>
2018-11-28 12:02:51 -0200

    Add files via upload

--- TelaEntrada.java
+++ TelaEntrada.java
@@ -4,12 +4,15 @@ import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
-import java.sql.Date;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.TimeZone;
+
 import javax.swing.ButtonGroup;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
@@ -43,6 +46,7 @@ public class TelaEntrada extends JFrame
     private JTextField data;
     private JLabel msgHora;
     private JTextField hora;
+
     
     private static final String[] tipos = {"", "Moto", "Carro", "Caminhonete"}; 
     int i, x;
@@ -56,11 +60,21 @@ public class TelaEntrada extends JFrame
         setLayout(new GridLayout(6, 1));
         JPanel[][] p = new JPanel[6][1];
         
+        SimpleDateFormat isoFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+        isoFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+        try {
+			Date date = isoFormat.parse("2010-05-23T09:01:02");
+		} catch (ParseException e1) {
+			// TODO Auto-generated catch block
+			//e1.printStackTrace();
+		}
+        
         Icon parada      = new ImageIcon(getClass().getResource("p.png"));
         Icon moto        = new ImageIcon(getClass().getResource("moto.png"));
         Icon carro       = new ImageIcon(getClass().getResource("carro.png"));
         Icon caminhonete = new ImageIcon(getClass().getResource("caminhonete.png"));
         
+       
         for(i = 0; i < 6; i++){
             p[i][0] = new JPanel();
             add(p[i][0]);
@@ -80,24 +94,54 @@ public class TelaEntrada extends JFrame
         msgPlaca = new JLabel("Placa", (int) CENTER_ALIGNMENT);
         msgPlaca.setFont(new Font("Arial", Font.PLAIN, 15));
         placa = new JTextField();
+        placa.setHorizontalAlignment(JTextField.CENTER);
+        
         msgModelo = new JLabel("Modelo", (int)CENTER_ALIGNMENT);
         msgModelo.setFont(new Font("Arial", Font.PLAIN, 15));
         modelo = new JTextField();
+        modelo.setHorizontalAlignment(JTextField.CENTER);
+        
         msgTipo = new JLabel("Tipo de veiculo:", (int) CENTER_ALIGNMENT);
         msgTipo.setFont(new Font("Arial", Font.PLAIN, 15));
         
         msgData = new JLabel("Data entrada", (int)CENTER_ALIGNMENT);
         msgData.setFont(new Font("Arial", Font.PLAIN, 15));
+        
         data = new JTextField("DD/MM/AAAA");
+        data.addFocusListener(new FocusListener() {
+			@Override
+			public void focusGained(FocusEvent e) {
+				data.setText("");
+			}
+			@Override
+			public void focusLost(FocusEvent e) {	
+				if(data.getText().equals(""))
+					data.setText("DD/MM/AAAA");
+			}
+        });
+  
         data.setHorizontalAlignment(JTextField.CENTER);
         msgHora = new JLabel("Horario de entrada", (int)CENTER_ALIGNMENT);
         msgHora.setFont(new Font("Arial", Font.PLAIN, 15));
+        
         hora = new JTextField(8);
         hora.setText("hh:mm:ss");
         hora.setHorizontalAlignment(JTextField.CENTER);
+        hora.addFocusListener(new FocusListener() {
+			@Override
+			public void focusGained(FocusEvent e) {
+				hora.setText("");
+			}
+			@Override
+			public void focusLost(FocusEvent e) {
+				if(hora.getText().equals(""))
+					hora.setText("hh:mm:ss");
+			}
+        });
         
         tipo = new JComboBox(tipos);
         tipo.setMaximumRowCount(4);
+        
         tipo.addItemListener(
                 new ItemListener()
                 {
@@ -125,7 +169,8 @@ public class TelaEntrada extends JFrame
                             break;
                         }
                     }
-                });    
+                });
+                
         p[1][0].add(msgPlaca);//
         p[1][0].add(msgModelo);//
         p[1][0].add(new JPanel());//
@@ -170,14 +215,6 @@ public class TelaEntrada extends JFrame
         
         p[4][0].setLayout(new GridLayout(1, 3));
         
-        /*hora.addItemListener(
-            new RadioButtonHandler(0));
-        mensalista.addItemListener(
-            new RadioButtonHandler(1));
-        pernoite.addItemListener(
-            new RadioButtonHandler(2));*/
-        
-        
         p[4][0].add(horaB);
         p[4][0].add(mensalista);
         p[4][0].add(pernoite);
@@ -194,7 +231,6 @@ public class TelaEntrada extends JFrame
         cancelar.addActionListener(list);
         cadastrar.addActionListener(list);
         
-
         horaB.addActionListener(list);
         mensalista.addActionListener(list);
         pernoite.addActionListener(list);
@@ -203,9 +239,8 @@ public class TelaEntrada extends JFrame
         p[5][0].add(cadastrar);
         
     }
-
         
-    private class Listener implements ActionListener //TO MEXENDO AQUUIIIIIII PRA VOCï¿½ LEMBRAR BRUNO SEU IDIOTA DATA
+    private class Listener implements ActionListener //TO MEXENDO AQUUIIIIIII PRA VOCÊ LEMBRAR BRUNO SEU IDIOTA DATA
     {
         @Override
         public void actionPerformed(ActionEvent e)
@@ -214,20 +249,31 @@ public class TelaEntrada extends JFrame
                 dispose();
             else if(e.getSource() == cadastrar)
             {
-            	int dia, mes, ano, hora, min, seg;
+            	int dia, mes, ano, hor, min, seg;
             	
-            	ano = Integer.parseInt(data.getText().substring(6, 10)); //AQUIIIIIIIIIII
-            	System.out.println(ano);
+            	ano = Integer.parseInt(data.getText().substring(6, 10));
+            	mes = Integer.parseInt(data.getText().substring(3, 5));
+            	dia = Integer.parseInt(data.getText().substring(0, 2));
             	
-            	//LocalDateTime d = new Date();
-            	
-            	//System.out.println(d);
+            	hor = Integer.parseInt(hora.getText().substring(0, 2));
+            	min = Integer.parseInt(hora.getText().substring(3, 5));
+            	seg = Integer.parseInt(hora.getText().substring(6, 8)); 	
+          
+//            	Parameters:
+//            		year the year minus 1900.
+//            		month the month between 0-11.
+//            		date the day of the month between 1-31.
+//            		hrs the hours between 0-23.
+//            		min the minutes between 0-59.
+//            		sec the seconds between 0-59.
+//            	
+            	Date d = new Date(ano - 1900, mes - 1, dia, hor, min, seg);
             	
                 SistemaEstacionamento sis = SistemaEstacionamento.getInstance();
-                sis.entradaVeiculo(placa.getText(), modelo.getText(), tipoVeiculo, tipoPacote, 1);
-            }
-
-            
+                sis.EntradaVeiculo(placa.getText(), modelo.getText(), tipoVeiculo, tipoPacote, d);
+                
+                dispose();
+            }     
             else if(e.getSource() == horaB)
             {
                 tipoPacote = "Hora";
